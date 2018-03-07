794539cfa6cefb47d5236fe7cc6b266095af2f73
Gerald <geraldjktan95@gmail.com>
2018-03-08 01:31:03 +0800

    MainActivity as LoginPage Added Homepage and Alerts Page

--- app/src/main/java/tomorrow/ntu/edu/sg/hospitalbees/Alerts.java
+++ app/src/main/java/tomorrow/ntu/edu/sg/hospitalbees/Alerts.java
@@ -3,11 +3,11 @@ package tomorrow.ntu.edu.sg.hospitalbees;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Confirm extends AppCompatActivity {
+public class Alerts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        setContentView(R.layout.activity_alerts);
     }
 }
--- app/src/main/java/tomorrow/ntu/edu/sg/hospitalbees/MainActivity.java
+++ app/src/main/java/tomorrow/ntu/edu/sg/hospitalbees/MainActivity.java
@@ -2,12 +2,30 @@ package tomorrow.ntu.edu.sg.hospitalbees;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    EditText editName, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName =(EditText) findViewById(R.id.editName);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+    }
+
+    public void clickHandler(View view) {
+        String name = editName.getText().toString();
+        String password = editPassword.getText().toString();
+
+        Log.i(TAG,"clickHandler: name~" + name + ", password~" + password);
+    }
+
+    public void clickHandler2(View view) {
     }
 }
